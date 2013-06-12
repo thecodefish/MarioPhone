5e1d0776a849d75dca9877a708ddee558910c96a
Simon Fisher <simon@bka.co.nz>
2013-06-12 18:01:36 +1200

    Initial commit - new mvc site

---
+++ MarioPhone/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Web.Mvc;
+
+namespace MarioPhone.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
