4facd492d3c3a29ae4a66853c06b33b3711b9bde
Simon Fisher <simon@bka.co.nz>
2013-06-12 18:39:50 +1200

    added signalr 'hello world'

--- MarioPhone/Global.asax.cs
+++ MarioPhone/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +12,8 @@ namespace MarioPhone
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapHubs();
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
---
+++ MarioPhone/Hubs/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace MarioPhone.Hubs
+{
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            // Call the addMessage method on all clients            
+            Clients.All.addMessage(message);
+        }
+    }
+}
