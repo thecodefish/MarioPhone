fb5636f6cadaedc1c5a448899365ba44f0dbfc21
Simon Fisher <thecodefish@gmail.com>
2013-06-13 21:52:48 +1200

    basic game control via mobile
    simple signalr messaging.
    mobile browser detection to mobile page.
    simple controls on mobile page.
    loading html5 mario, sending key events based on signalr messages.

---
+++ MarioPhone/Controllers/MobileController.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Web.Mvc;
+
+namespace MarioPhone.Controllers
+{
+    public class MobileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
